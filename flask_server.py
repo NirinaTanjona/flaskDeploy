7662653b8f6e6616b6bfd141fdc2df4366ad47cc
NirinaTanjona <tanjonawork@gmail.com>
2021-02-11 13:19:13 +0300

    add a variable a and a comment

--- flask_server.py
+++ flask_server.py
@@ -2,7 +2,10 @@ import flask
 
 # I learn git
 print('hello world')
+a = 12
+print(a)
 print('hello again')
 print('hello third time')
 print('hello one more time')
 # One more comment
+# Add a more fucking new line
