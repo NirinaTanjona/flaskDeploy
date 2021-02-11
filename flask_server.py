bb7820005110dd8a39e50a876f80e6ddd863ba8c
NirinaTanjona <48290392+NirinaTanjona@users.noreply.github.com>
2021-02-11 14:37:11 +0300

    Update flask_server.py

--- flask_server.py
+++ flask_server.py
@@ -7,11 +7,13 @@ print('hello third time')
 print('hello one more time')
 
 for i in range(10):
+    # iter through 0 to 10
     print("iteration : {}".format(i))
 
 print('add a new feature')
 
 # a class that is doing anything
+
 class ABCthatDoingAnything():
     '''this is a comment'''
     pass
