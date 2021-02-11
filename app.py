48057d4c513bcfb17c4b313fd39e2028d421f046
NirinaTanjona <tanjonawork@gmail.com>
2021-02-11 18:11:52 +0300

    add tensorflow import

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import tensorflow as tf
 
 
 app = Flask(__name__)
@@ -6,4 +7,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "hello, world"
+    return 'hello world'
