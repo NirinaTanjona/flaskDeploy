0dd5148d60b8de1c43fc9b80415e723f22523478
NirinaTanjona <tanjonawork@gmail.com>
2021-02-11 16:02:23 +0300

    flask_server.py

--- flask_server.py
+++ flask_server.py
@@ -1,24 +1,5 @@
 import flask
 
-# I learn git
-print('hello world, hello mars, hello moon')
-a = 12
-print(a)
-# I place comment here
-print('hello again')
-print('hello third time')
-# I place a comment here
-print('hello one more time')
-for i in range(10):
-    # iter through 0 to 10
-    print("iteration : {}".format(i))
-
-print('add a new feature')
-
-
-# a class that is doing anything
-class ABCthatDoingAnything():
-    '''this is a comment'''
-    pass
-# One more comment
-# Add a more fucking new line
+# this part of the code will say hello to the user
+# say hello in python
+print('hello you')
