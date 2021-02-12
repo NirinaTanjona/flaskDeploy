eda0e55ba812098fc3f3f49b23b9c563b41aeb5a
NirinaTanjona <tanjonawork@gmail.com>
2021-02-12 08:41:00 +0300

    make hello world into a dict

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 import tensorflow as tf
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route("/")
+@app.route("/hello")
 def index():
-    return 'hello world'
+    return {'result': "Hello world"}
